fe14abd7e4111b8369c58369e5cc31cf23f684ce
Oscar Roche <oscar.w.roche@gmail.com>
2024-04-22 19:24:41 +0200

    :bricks: replace extension by closure

--- src/application/queries/get_latest_block.rs
+++ src/application/queries/get_latest_block.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
 
-use axum::{response::IntoResponse, Extension};
+use axum::response::IntoResponse;
 
 use crate::domain::services::blockchain_polling_service::BlockchainPollingService;
 
 pub async fn get_latest_block_query(
-    Extension(blockchain_polling_service): Extension<Arc<dyn BlockchainPollingService>>,
+    blockchain_polling_service: Arc<dyn BlockchainPollingService>,
 ) -> impl IntoResponse {
     blockchain_polling_service.get_latest_block().await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use application::queries::get_latest_block::get_latest_block_query;
-use axum::{routing::get, Extension, Router};
+use axum::{routing::get, Router};
 use infrastructure::adapters::op_stack_adapter::OpStackAdapter;
 
 pub mod application;
@@ -13,10 +13,17 @@ const RPC_URL: &str = "http://localhost:8545";
 #[tokio::main]
 async fn main() {
     let blockchain_polling_service = Arc::new(OpStackAdapter::new(RPC_URL));
-    let app = Router::new()
-        .route("/block", get(get_latest_block_query))
-        .layer(Extension(blockchain_polling_service));
+    let app = Router::new().route(
+        "/block",
+        get({
+            let blockchain_polling_service = Arc::clone(&blockchain_polling_service);
+            move || get_latest_block_query(blockchain_polling_service)
+        }),
+    );
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
     axum::serve(listener, app).await.unwrap();
 }
